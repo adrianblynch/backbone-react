92a4b991c9f0e79cfd444035ffa39fe808d981c8
Gaspar Rey <info@gasparrey.com>
2015-03-24 22:24:30 +0000

    Correct backbone mixin

--- app/scripts/util/backboneMixin.js
+++ app/scripts/util/backboneMixin.js
@@ -9,10 +9,11 @@ define(function () {
             // Whenever there may be a change in the Backbone data, trigger a
             // reconcile.
             this.getBackboneCollections().forEach(function (collection) {
+                console.log(this);
                 // explicitly bind `null` to `forceUpdate`, as it demands a callback and
                 // React validates that it's a function. `collection` events passes
                 // additional arguments that are not functions
-                collection.on('add remove change reset', this.forceUpdate.bind(this, null));
+                collection.on('add remove change reset', this.forceUpdate.bind(this, null), this);
             }, this);
         },
 
@@ -20,6 +21,7 @@ define(function () {
             // Ensure that we clean up any dangling references when the component is
             // destroyed.
             this.getBackboneCollections().forEach(function (collection) {
+                console.log('C:', collection);
                 collection.off(null, null, this);
             }, this);
         }
