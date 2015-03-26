ff4f21ad7596621faafe0621f9f1efc1d5c50504
Gaspar Rey <info@gasparrey.com>
2015-03-26 01:17:22 +0000

    Improve the backbone mixin

--- app/scripts/util/backboneMixin.js
+++ app/scripts/util/backboneMixin.js
@@ -8,22 +8,35 @@ define(function () {
         componentDidMount: function () {
             // Whenever there may be a change in the Backbone data, trigger a
             // reconcile.
-            this.getBackboneCollections().forEach(function (collection) {
-                console.log(this);
-                // explicitly bind `null` to `forceUpdate`, as it demands a callback and
-                // React validates that it's a function. `collection` events passes
-                // additional arguments that are not functions
-                collection.on('add remove change reset', this.forceUpdate.bind(this, null), this);
-            }, this);
+            if (this.getBackboneCollections) {
+                this.getBackboneCollections().forEach(function (collection) {
+                    // explicitly bind `null` to `forceUpdate`, as it demands a callback and
+                    // React validates that it's a function. `collection` events passes
+                    // additional arguments that are not functions
+                    collection.on('add remove change reset', this.forceUpdate.bind(this, null), this);
+                }, this);
+            }
+            if (this.getBackboneModels) {
+                this.getBackboneModels().forEach(function (model) {
+                    model.on('change', this.forceUpdate.bind(this, null), this);
+                }, this);
+            }
         },
 
         componentWillUnmount: function () {
             // Ensure that we clean up any dangling references when the component is
             // destroyed.
-            this.getBackboneCollections().forEach(function (collection) {
-                console.log('C:', collection);
-                collection.off(null, null, this);
-            }, this);
+            if (this.getBackboneCollections) {
+                this.getBackboneCollections().forEach(function (collection) {
+                    collection.off(null, null, this);
+                }, this);
+            }
+            if (this.getBackboneModels) {
+                this.getBackboneModels().forEach(function (model) {
+                    model.off(null, null, this);
+                }, this);
+            }
+
         }
 
     };
